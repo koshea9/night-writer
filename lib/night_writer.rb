dcee5ee6490587f8aeaba6efec284a5e1f7cc49d
koshea9 <keegan.oshea9@gmail.com>
2021-03-05 14:44:57 -0700

    Add dynamic CLI message

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,14 +1,12 @@
-# when we run this $ ruby ./lib/night_writer.rb message.txt braille.txt
-# is printed to the terminal Created 'braille.txt' containing 256 characters
-
 handle = File.open(ARGV[0], 'r')
 incoming_text = handle.read
+character_count = incoming_text.strip.length
 handle.close
-
-puts "Created 'braille.txt' containing 256 characters"
 
 translated_file = incoming_text.gsub(/./) {|s| s.ord.to_s + ' '}
 writer = File.open(ARGV[1], 'w')
+writer_file = ARGV[1].to_s
 
 writer.write(translated_file)
 writer.close
+puts "Created '#{writer_file}' containing #{character_count} characters"

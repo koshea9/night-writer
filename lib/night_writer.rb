943c89034d5ecddcb4dfcec6b7acae1087a7f2e1
koshea9 <keegan.oshea9@gmail.com>
2021-03-06 10:38:10 -0700

    WIP 80 english character break

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -6,19 +6,42 @@ incoming_text = input_file.read.chomp.downcase
 input_file.close
 english_dictionary = EnglishDictionary.new
 
-#need to write left to right vs. up/down
-translation = english_dictionary.translate_message(incoming_text)
-      row1 = translation.flat_map do |set|
-        set[0]
-      end.join(', ').gsub(",","")
-      row2 = translation.flat_map do |set|
-        set[1]
-      end.join(', ').gsub(",","")
-      row3 = translation.flat_map do |set|
-        set[2]
-      end.join(', ').gsub(",","")
 
+translation = english_dictionary.translate_message(incoming_text)
+row1_count = 0
+row2_count = 0
+row3_count = 0
+row1_index = 0
+row2_index = 1
+row3_index = 2
+  #loop
+      row1 = translation.flat_map do |set|
+        row1_count += 1
+        set[row1_index]
+      end.join(', ').gsub(",","")
+      if row1_count % 40
+        row1_index += 1
+      end
+
+      row2 = translation.flat_map do |set|
+        row2_count += 1
+        set[row2_index]
+      end.join(', ').gsub(",","")
+      if row2_count % 40
+        row2_index += 1
+      end
+
+      row3 = translation.flat_map do |set|
+        row3_count += 1
+        set[row3_index]
+      end.join(', ').gsub(",","")
+      if row3_count % 40
+        row3_index += 1
+    end
+require "pry"; binding.pry
+# loop printing row 1 2 3 until 40 characters then start new row
 print_view = "#{row1}\n#{row2}\n#{row3}"
+
 writer = File.open(ARGV[1], "w")
 writer.write(print_view)
 writer.close

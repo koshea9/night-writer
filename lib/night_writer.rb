f826ba00ce81963fd170ea5f3bcca0477a415603
koshea9 <keegan.oshea9@gmail.com>
2021-03-07 09:59:58 -0700

    Refactor i/o files and fix spelling in message file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,19 +1,9 @@
-require './lib/dictionary'
 require './lib/translator'
 
-input_file = File.open(ARGV[0], "r")
-incoming_text = input_file.read.chomp.downcase
+input_file = File.read(ARGV[0]).chomp.downcase
 
-input_file.close
-translator = Translator.new(incoming_text)
+translator = Translator.new(input_file)
 
-translation_to_braille = translator.output_to_file
+translation_to_braille = File.write(ARGV[1], translator.output_to_file)
 
-writer = File.open(ARGV[1], "w")
-writer.write(translation_to_braille)
-writer.close
-
-character_count = incoming_text.length
-
-output_file = ARGV[1]
-  puts "Created #{output_file} containing #{character_count} characters"
+puts "Created #{ARGV[1]} containing #{input_file.length} characters"

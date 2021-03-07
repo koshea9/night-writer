8386fb274236a9b03b88bc840ededf7966f40ad8
koshea9 <keegan.oshea9@gmail.com>
2021-03-07 14:12:07 -0700

    Add test for converting multiple english letters to braille

--- test/translator_to_english_test.rb
+++ test/translator_to_english_test.rb
@@ -3,17 +3,21 @@ require './test/test_helper'
 class TranslatorToEnglishTest < Minitest::Test
 
   def test_it_exists
-    translator_to_english = TranslatorToEnglish.new("0.
-    ..
-    ..")
+    translator_to_english = TranslatorToEnglish.new("0.\n..\n..")
 
     assert_instance_of TranslatorToEnglish, translator_to_english
   end
 
-  def test_it_can_translate_single_character
+  def test_it_can_translate_single_braille_character_to_english
     translator_to_english = TranslatorToEnglish.new("0.\n..\n..")
 
     assert_equal "a", translator_to_english.translate_braille_message
+  end
+
+  def test_it_can_convert_multipe_braille_characters_to_english
+    translator_to_english = TranslatorToEnglish.new("0.00..\n0..0..\n0.0.0.\n0.0.0.\n0..00.")
+
+    assert_equal "hello", translator_to_english.translate_braille_message
   end
 
 end

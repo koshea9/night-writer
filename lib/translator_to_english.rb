039f08e62fa48fc257a6ed85ccf92d02ce27b510
koshea9 <keegan.oshea9@gmail.com>
2021-03-07 18:56:44 -0700

    Add incoming braille message breakout

--- lib/translator_to_english.rb
+++ lib/translator_to_english.rb
@@ -10,7 +10,22 @@ class TranslatorToEnglish
     @braille_dictionary = dictionary.braille_dictionary
   end
 
-  def translate_braille_message
-      @braille_dictionary[message.split]
+    def translate_braille_message
+      @braille_dictionary[@message.split]
+    end
+
+  def top_braille_message
+    array = message.scan(/.{6}/)
+    top = array [0]
+  end
+
+  def middle_braille_message
+    array = message.scan(/.{6}/)
+    top = array [1]
+  end
+
+  def bottom_braille_message
+    array = message.scan(/.{6}/)
+    top = array [2]
   end
 end
--- test/translator_to_english_test.rb
+++ test/translator_to_english_test.rb
@@ -14,6 +14,17 @@ class TranslatorToEnglishTest < Minitest::Test
     assert_equal "h", translator_to_english.translate_braille_message
   end
 
+  def test_it_can_convert_input_to_top_middle_bottom
+    translator_to_english = TranslatorToEnglish.new("0.0.0.0.0.\n00.00.0..0\n....0.0.0.\n")
+
+    assert_equal "0.0.0.", translator_to_english.top_braille_message #take 0 index
+    assert_equal "00.00.", translator_to_english.middle_braille_message #take 1 index
+    assert_equal "....0.", translator_to_english.bottom_braille_message# take 2 index
+
+    # hello world this is a longer string to test "0.0.0.0.0....00.0.0.00...00..0.0...0.0..0...0.0.00000.0....0.00..00000..0.00...0\n00.00.0..0..00.0000..0..00000.0...0.0.......0..0.000.000..0.00000..000...00...00\n....0.0.0....00.0.0.....0.....0.....0.......0.0.0.....0...0.0.0...0.....0.....0.\n0.00.0\n.0..00\n..000.\n"
+  end
+
+
   def test_it_can_convert_multipe_braille_characters_to_english
     translator_to_english = TranslatorToEnglish.new("0.0.0.0.0.\n00.00.0..0\n....0.0.0.\n")
 

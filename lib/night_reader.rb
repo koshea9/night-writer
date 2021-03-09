998224ee099ead7829521fc8a8386fb0ce6482b1
koshea9 <keegan.oshea9@gmail.com>
2021-03-08 18:50:19 -0700

    Fix character count to exclude counting line breaks

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -6,4 +6,4 @@ translator_to_english = TranslatorToEnglish.new(input_file)
 
 translation_to_english = File.write(ARGV[1], translator_to_english.translate_braille_message)
 
-puts "Created #{ARGV[1]} containing #{input_file.length} characters"
+puts "Created #{ARGV[1]} containing #{input_file.delete("\n").length} characters"

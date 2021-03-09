b6a3acf0d51bdc8adde086ce2b49d37b6f26337a
koshea9 <keegan.oshea9@gmail.com>
2021-03-08 17:56:29 -0700

    Add additional test for formatting incoming messsage longer than 80 braille characters

--- test/translator_to_english_test.rb
+++ test/translator_to_english_test.rb
@@ -16,8 +16,10 @@ class TranslatorToEnglishTest < Minitest::Test
 
   def test_it_can_format_braille_message_to_match_characters
     translator_to_english = TranslatorToEnglish.new("0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n")
+    translator_to_english2 = TranslatorToEnglish.new("0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n")
 
     assert_equal ["0.00..","0..0..","0.0.0.","0.0.0.","0..00.","......",".000.0","0..00.","0.000.","0.0.0.","00.0.."], translator_to_english.format_braille_message
+    assert_equal ["0.00..","0..0..","0.0.0.","0.0.0.","0..00.","......",".000.0","0..00.","0.000.","0.0.0.","00.0..","0.00..","0..0..","0.0.0.","0.0.0.","0..00.","......",".000.0","0..00.","0.000.","0.0.0.","00.0.."], translator_to_english2.format_braille_message
   end
 
   def test_it_can_convert_multipe_braille_characters_to_english
@@ -29,9 +31,11 @@ class TranslatorToEnglishTest < Minitest::Test
   end
 
   def test_it_can_out_put_to_file_based_on_length
-    translator_to_english = TranslatorToEnglish.new("0.\n00\n..")
+    translator_to_english1 = TranslatorToEnglish.new("0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n")
+    translator_to_english2 = TranslatorToEnglish.new("0.\n00\n..")
 
-    assert_equal "h", translator_to_english.output_to_file
+    assert_equal "hello world", translator_to_english1.output_to_file
+    assert_equal "h", translator_to_english2.output_to_file
   end
 
   def test_it_can_return_length_excluding_line_breaks
